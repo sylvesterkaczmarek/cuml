08f090d8c318dfab977d2261f6da116f07208460
Simon Adorf <sadorf@nvidia.com>
2026-09-08 07:54:18 -0500

    Remove unrelated documentation config change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -289,8 +289,10 @@ linkcode_resolve = make_linkcode_resolve(
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-default_role
 default_role = "py:obj"
 
-# Define the :py: role globally so inline Python references render
-# consistently across documentation pages.
+# rst_prolog is prepended to every RST file. We define the :py: role here so
+# that docstrings using :py:`code` for inline Python-highlighted code render
+# correctly on all pages. This role was previously defined only in the old
+# monolithic api.rst.
 rst_prolog = """
 .. role:: py(code)
    :language: python
